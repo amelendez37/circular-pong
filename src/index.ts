787fab9434d1924f9df21622274bb5794b4aaea7
Aaron Melendez <aaron.melendez37@gmail.com>
2025-06-20 16:44:26 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,19 @@ function run() {
     // gives us methods to draw within our 2d canvas element
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
+    // const dpi = window.devicePixelRatio || 1;
+    const style = getComputedStyle(canvas);
+    const cssWidth = parseFloat(style.width);
+    const cssHeight = parseFloat(style.height);
+    canvas.width = cssWidth;
+    canvas.height = cssHeight;
+
+    // // Scale context to handle drawing at higher resolution
+    // ctx.scale(dpi, dpi);
+
     // Begin a new path
     ctx.beginPath();
-    ctx.arc(50, 50, 50, 0, Math.PI * 2);
+    ctx.arc(50, 50, 20, 0, Math.PI * 2);
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
     ctx.stroke(); // Outline the circle
