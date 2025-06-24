af119c430e9dbe38696d5401ad8b0e962a38c804
Aaron Melendez <aaron.melendez37@gmail.com>
2025-06-23 17:15:51 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ function run() {
     function drawPlayerOne() {
         console.log(canvas.height)
         ctx.beginPath();
-        ctx.arc(canvas.width / 2, canvas.height / 2 - 7, 240, 1.309, 1.8326);
+        ctx.arc(canvas.width / 2, canvas.height / 2 - 7, 240, 1.309, 1.8326); // x, y, radius, 75deg, 105deg
         ctx.lineWidth = 6;
         ctx.strokeStyle = 'green';
         ctx.stroke();
